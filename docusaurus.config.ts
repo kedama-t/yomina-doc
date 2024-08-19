90cacf420c1d1a08a1d50d0246209fbc70e551df
kedama-t <114888622+kedama-t@users.noreply.github.com>
2024-08-19 22:44:49 +0900

    設定の修正

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,11 +4,11 @@ import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
   title: "YOMINA",
-  tagline: "Dinosaurs are cool",
+  tagline: "YOMINAのドキュメント",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://docs.yomina.app",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
@@ -49,7 +49,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/icon.png",
     navbar: {
       title: "YOMINA - Doc",
       logo: {
